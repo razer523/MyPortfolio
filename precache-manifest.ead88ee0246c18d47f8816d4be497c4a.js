a0f3ff379d8788fbcf4a0109f12a959ceebdfffe
Raymond Albright <67028365+razer523@users.noreply.github.com>
2021-01-06 15:36:40 -0800

    Updates

--- precache-manifest.ead88ee0246c18d47f8816d4be497c4a.js
+++ precache-manifest.ead88ee0246c18d47f8816d4be497c4a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "c0cef9d1e49e9fb80bdc",
+    "revision": "c8c4af40c6446c44a0f1",
     "url": "/MyPortfolio/static/css/main.7df22b18.chunk.css"
   },
   {
-    "revision": "c0cef9d1e49e9fb80bdc",
-    "url": "/MyPortfolio/static/js/main.c0cef9d1.chunk.js"
+    "revision": "c8c4af40c6446c44a0f1",
+    "url": "/MyPortfolio/static/js/main.c8c4af40.chunk.js"
   },
   {
     "revision": "ae5132617e7538071453",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/MyPortfolio/static/js/2.5ec21d73.chunk.js"
   },
   {
-    "revision": "72351ff3c93c283210956126e237a4e3",
+    "revision": "81c3e21a14f801a772a07e426ab4e021",
     "url": "/MyPortfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyPortfolio/precache-manifest.f3968be00a56a4176b2222df212064eb.js"
+  "/MyPortfolio/precache-manifest.ead88ee0246c18d47f8816d4be497c4a.js"
 );
 
 workbox.clientsClaim();
