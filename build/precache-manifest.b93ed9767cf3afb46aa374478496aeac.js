68cf511ceed44032319bc1f4e6b3f0fd30ed84fb
Raymond Albright <67028365+razer523@users.noreply.github.com>
2021-01-06 13:31:44 -0800

    test pull

---
+++ build/precache-manifest.b93ed9767cf3afb46aa374478496aeac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a2cd0a6144f83e35bb2",
+    "url": "/MyPortfolio/static/css/main.4deed863.chunk.css"
+  },
+  {
+    "revision": "8a2cd0a6144f83e35bb2",
+    "url": "/MyPortfolio/static/js/main.8a2cd0a6.chunk.js"
+  },
+  {
+    "revision": "ae5132617e7538071453",
+    "url": "/MyPortfolio/static/js/runtime~main.ae513261.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/MyPortfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "fd919e5093ead9a47ac1c7d8768fbf1d",
+    "url": "/MyPortfolio/index.html"
+  }
+];
