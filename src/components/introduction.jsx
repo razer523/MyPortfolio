5ff82241e4904f87eacaf477b0f390cb068733eb
Raymond Albright <67028365+razer523@users.noreply.github.com>
2021-01-15 11:01:03 -0800

    changed logo background color

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -6,21 +6,21 @@ export default class Introduction extends Component {
       <div>
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
-            <ul className="slides">              <li style={{backgroundImage: 'url(images/my_logo.jpg)'}}>
+            <ul className="slides">              <li style={{backgroundImage: 'url(images/my_logo2.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>I'm Raymond Albright <br/>   Full Stack Developer</h1>
+                          <h1><br/> <br/> I'm Raymond Albright <br/>   Full Stack Developer</h1>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/my_logo.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/my_logo2.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
@@ -35,14 +35,14 @@ export default class Introduction extends Component {
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/my_logo.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/my_logo2.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>View my GitHub repositories</h1>
+                          <h1>View my GitHub <br/> repositories</h1>
                           <p><a className="btn btn-primary btn-learn" href="https://github.com/razer523?tab=repositories" target="_blank" rel="noopener noreferrer">Projects <i className="icon-briefcase3" /></a></p>
                         </div>
                       </div>
